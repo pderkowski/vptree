e54c5d43a3c928e9fc2311c2750e849971db610c
Piotr Derkowski <piotder@gmail.com>
2017-02-10 11:15:53 +0100

    First version and tests.

---
+++ test/test_vptree.cpp
@@ -0,0 +1,138 @@
+#define CATCH_CONFIG_MAIN
+
+#include "vptree.hpp"
+#include "catch.hpp"
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <cmath>
+#include <set>
+#include <algorithm>
+#include <array>
+#include <queue>
+
+template<typename Matrix>
+void fillPoints(Matrix& m) {
+    typedef typename Matrix::value_type Column;
+    std::default_random_engine rng(0);
+    std::uniform_real_distribution<double> dist(0., 1.);
+
+    for (int i = 0; i < m.size(); ++i) {
+        for (int j = 0; j < m[i].size(); ++j) {
+            m[i][j] = dist(rng);
+        }
+    }
+}
+
+class NaiveNearestNeighbors {
+public:
+    typedef std::vector<double> Vector;
+    typedef std::vector<Vector> Matrix;
+
+    NaiveNearestNeighbors(const Matrix& points)
+    : points_(points)
+    { }
+
+    std::vector<vpt::Result<Vector>> getNearestNeighbors(const Vector& target, int neighborsCount) const {
+        std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, std::greater<std::pair<double, int>>> q;
+        vpt::EuclideanMetric<Vector> metric;
+
+        for (int i = 0; i < points_.size(); ++i) {
+            auto dist = metric(points_[i], target);
+            q.push(std::make_pair(dist, i));
+        }
+
+        std::vector<vpt::Result<Vector>> results;
+
+        while (neighborsCount--) {
+            auto top = q.top();
+            q.pop();
+
+            results.push_back(vpt::Result<Vector>{&(points_[top.second]), top.second, top.first});
+        }
+
+        return results;
+    }
+
+private:
+    std::vector<std::vector<double>> points_;
+};
+
+TEST_CASE("Distance: exact") {
+    std::vector<double> v1(10000, 0);
+    std::vector<double> v2(10000, 1);
+    vpt::EuclideanMetric<std::vector<double>> metric;
+    REQUIRE(metric(v1, v2) == 100.);
+}
+
+TEST_CASE("Distance: approximate") {
+    std::vector<double> v1(10000, 0);
+    std::vector<double> v2(10000, 0.1);
+    vpt::EuclideanMetric<std::vector<double>> metric;
+    REQUIRE(metric(v1, v2) == Approx(10.));
+}
+
+TEST_CASE("VpTree can be constructed from a vector, std::array or initializer list") {
+    auto p1 = std::vector<std::vector<double>>(1000, std::vector<double>(100));
+    fillPoints(p1);
+
+    auto p2 = std::array<std::array<double, 100>, 1000>();
+    fillPoints(p2);
+
+    vpt::VpTree<std::vector<double>> t1(p1);
+    vpt::VpTree<std::vector<double>> t2(p1.begin(), p1.end());
+
+    vpt::VpTree<std::array<double, 100>> t3(p2);
+    vpt::VpTree<std::array<double, 100>> t4(p2.begin(), p2.end());
+
+    vpt::VpTree<std::vector<double>> t5({
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    });
+}
+
+TEST_CASE("VpTree::getNearestNeighbors works in simple case") {
+    auto points = std::vector<std::vector<double>>{
+        {0, 0, 1},
+        {1, 1, 1},
+        {2, 0, 0},
+        {-1, -1, 0},
+        {10, 0, 5}
+    };
+
+    vpt::VpTree<std::vector<double>> t1(points);
+
+    auto results = t1.getNearestNeighbors({ 0, 0, 0 }, 3);
+    REQUIRE(results.size() == 3);
+
+    REQUIRE(results[0].index == 0);
+    REQUIRE(*results[0] == points[0]);
+    REQUIRE(results[0].dist == 1);
+
+    REQUIRE(results[1].index == 3);
+    REQUIRE(*results[1] == points[3]);
+    REQUIRE(results[1].dist == std::sqrt(2));
+
+    REQUIRE(results[2].index == 1);
+    REQUIRE(*results[2] == points[1]);
+    REQUIRE(results[2].dist == std::sqrt(3));
+}
+
+TEST_CASE("VpTree::getNearestNeighbors returns the same results as naive method for a larger case") {
+    auto points = std::vector<std::vector<double>>(1000, std::vector<double>(100));
+    fillPoints(points);
+
+    auto tree = vpt::VpTree<std::vector<double>>(points);
+    auto naive = NaiveNearestNeighbors(points);
+
+    for (const auto& p : points) {
+        const auto neighborsCount = 10;
+        auto treeResults = tree.getNearestNeighbors(p, neighborsCount);
+        auto naiveResults = naive.getNearestNeighbors(p, neighborsCount);
+
+        for (int i = 0; i < neighborsCount; ++i) {
+            REQUIRE(treeResults[i] == naiveResults[i]);
+        }
+    }
+}
---
+++ vptree.hpp
@@ -0,0 +1,267 @@
+// Based on http://stevehanov.ca/blog/index.php?id=130 by Steve Hanov
+
+#pragma once
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <random>
+#include <cmath>
+
+namespace vpt {
+
+template<class InputIterator>
+double sum(InputIterator begin, InputIterator end) {
+    double result = 0;
+    for (; begin != end; ++begin) {
+        result += *begin;
+    }
+    return result;
+}
+
+template<typename T>
+struct EuclideanMetric {
+    double operator() (const T& v1, const T& v2) const {
+        std::vector<double> diffSquares(v1.size());
+        std::transform(v1.begin(), v1.end(), v2.begin(), diffSquares.begin(), [] (double lhs, double rhs) {
+            return (lhs - rhs) * (lhs - rhs);
+        });
+        auto sum = vpt::sum(diffSquares.begin(), diffSquares.end());
+        return std::sqrt(sum);
+    }
+};
+
+
+
+template<typename T, typename Metric>
+class Searcher;
+
+
+template<typename T>
+struct Result {
+    const T* item;
+    int index;
+    double dist;
+
+    const T& operator* () const {
+        return *item;
+    }
+
+    bool operator == (const Result<T>& other) const {
+        return *item == *(other.item)
+            && index == other.index
+            && dist == other.dist;
+    }
+};
+
+
+
+template<typename T, typename Metric = EuclideanMetric<T>>
+class VpTree {
+public:
+    template<typename InputIterator>
+    explicit VpTree(InputIterator start, InputIterator end, Metric metric = Metric());
+
+    template<typename Container>
+    explicit VpTree(const Container& container, Metric metric = Metric());
+
+    explicit VpTree(std::initializer_list<T> list, Metric metric = Metric());
+
+    std::vector<Result<T>> getNearestNeighbors(const T& target, int neighborsCount) const;
+    const Metric getDistance;
+
+private:
+    struct Node {
+        static const int Leaf = -1;
+
+        Node(int item, double threshold = 0., int left = Leaf, int right = Leaf)
+        : item(item), threshold(threshold), left(left), right(right)
+        { }
+
+        int item;
+        double threshold;
+        int left;
+        int right;
+    };
+
+private:
+    typedef std::pair<T, int> ItemType;
+
+    std::vector<ItemType> items_;
+    std::vector<Node> nodes_;
+
+    std::mt19937 rng_;
+
+    template<typename InputIterator>
+    std::vector<ItemType> makeItems(InputIterator start, InputIterator end);
+
+    int makeTree(int lower, int upper);
+    void selectRoot(int lower, int upper);
+    void partitionByDistance(int lower, int pos, int upper);
+    int makeNode(int item);
+    Node root() const { return nodes_[0]; }
+
+    friend class Searcher<T, Metric>;
+};
+
+template<typename T, typename Metric>
+template<typename InputIterator>
+VpTree<T, Metric>::VpTree(InputIterator start, InputIterator end, Metric metric)
+: getDistance(metric), items_(makeItems(start, end)), nodes_(), rng_() {
+    std::random_device rd;
+    rng_.seed(rd());
+    nodes_.reserve(items_.size());
+    makeTree(0, items_.size());
+}
+
+template<typename T, typename Metric>
+template<typename Container>
+VpTree<T, Metric>::VpTree(const Container& container, Metric metric)
+: VpTree(container.begin(), container.end(), metric)
+{ }
+
+template<typename T, typename Metric>
+VpTree<T, Metric>::VpTree(std::initializer_list<T> list, Metric metric)
+: VpTree(list.begin(), list.end(), metric)
+{ }
+
+template<typename T, typename Metric>
+int VpTree<T, Metric>::makeTree(int lower, int upper) {
+    if (lower >= upper) {
+        return Node::Leaf;
+    } else if (lower + 1 == upper) {
+        return makeNode(lower);
+    } else {
+        selectRoot(lower, upper);
+        int median = (upper + lower) / 2;
+        partitionByDistance(lower, median, upper);
+        auto node = makeNode(lower);
+        nodes_[node].threshold = getDistance(items_[lower].first, items_[median].first);
+        nodes_[node].left = makeTree(lower + 1, median);
+        nodes_[node].right = makeTree(median, upper);
+        return node;
+    }
+}
+
+template<typename T, typename Metric>
+void VpTree<T, Metric>::selectRoot(int lower, int upper) {
+    std::uniform_int_distribution<int> uni(lower, upper - 1);
+    int root = uni(rng_);
+    std::swap(items_[lower], items_[root]);
+}
+
+template<typename T, typename Metric>
+void VpTree<T, Metric>::partitionByDistance(int lower, int pos, int upper) {
+    std::nth_element(
+        items_.begin() + lower + 1,
+        items_.begin() + pos,
+        items_.begin() + upper,
+        [lower, this] (const ItemType& i1, const ItemType& i2) {
+            return getDistance(items_[lower].first, i1.first) < getDistance(items_[lower].first, i2.first);
+        });
+}
+
+template<typename T, typename Metric>
+int VpTree<T, Metric>::makeNode(int item) {
+    nodes_.push_back(Node(item));
+    return nodes_.size() - 1;
+}
+
+template<typename T, typename Metric>
+template<typename InputIterator>
+std::vector<std::pair<T, int>> VpTree<T, Metric>::makeItems(InputIterator begin, InputIterator end) {
+    std::vector<std::pair<T, int>> res;
+    for (int i = 0; begin != end; ++begin, ++i) {
+        res.push_back(std::make_pair(*begin, i));
+    }
+    return res;
+}
+
+template<typename T, typename Metric>
+std::vector<Result<T>> VpTree<T, Metric>::getNearestNeighbors(const T& target, int neighborsCount) const {
+    Searcher<T, Metric> searcher(this, target, neighborsCount);
+    return searcher.search();
+}
+
+
+
+template<typename T, typename Metric>
+class Searcher {
+public:
+    explicit Searcher(const VpTree<T, Metric>* tree, const T& target, int neighborsCount);
+
+    std::vector<Result<T>> search();
+
+private:
+    typedef typename VpTree<T, Metric>::Node Node;
+
+    struct HeapItem {
+        bool operator < (const HeapItem& other) const {
+            return dist < other.dist;
+        }
+
+        int item;
+        double dist;
+    };
+
+private:
+    void searchInNode(const Node& node);
+
+    const VpTree<T, Metric>* tree_;
+    T target_;
+    int neighborsCount_;
+    double tau_;
+    std::priority_queue<HeapItem> heap_;
+};
+
+template<typename T, typename Metric>
+Searcher<T, Metric>::Searcher(const VpTree<T, Metric>* tree, const T& target, int neighborsCount)
+: tree_(tree), target_(target), neighborsCount_(neighborsCount), tau_(std::numeric_limits<double>::max()), heap_()
+{ }
+
+template<typename T, typename Metric>
+std::vector<Result<T>> Searcher<T, Metric>::search() {
+    searchInNode(tree_->root());
+
+    std::vector<Result<T>> results;
+    while(!heap_.empty()) {
+        auto item = heap_.top().item;
+        auto result = Result<T>{&(tree_->items_[item].first), tree_->items_[item].second, heap_.top().dist};
+        results.push_back(result);
+        heap_.pop();
+    }
+    std::reverse(results.begin(), results.end());
+    return results;
+}
+
+template<typename T, typename Metric>
+void Searcher<T, Metric>::searchInNode(const Node& node) {
+    double dist = tree_->getDistance(tree_->items_[node.item].first, target_);
+
+    if (dist < tau_) {
+        if (heap_.size() == neighborsCount_)
+            heap_.pop();
+
+        heap_.push(HeapItem{node.item, dist});
+
+        if (heap_.size() == neighborsCount_)
+            tau_ = heap_.top().dist;
+    }
+
+    if (dist < node.threshold) {
+        if (node.left != Node::Leaf && dist - tau_ <= node.threshold)
+            searchInNode(tree_->nodes_[node.left]);
+
+        if (node.right != Node::Leaf && dist + tau_ >= node.threshold)
+            searchInNode(tree_->nodes_[node.right]);
+    } else {
+        if (node.right != Node::Leaf && dist + tau_ >= node.threshold)
+            searchInNode(tree_->nodes_[node.right]);
+
+        if (node.left != Node::Leaf && dist - tau_ <= node.threshold)
+            searchInNode(tree_->nodes_[node.left]);
+    }
+}
+
+}
